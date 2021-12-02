d67f0a84525b5c62814a5c2867e22b995e6d4876
kieranmacdonald <kieranmacdonaldschool@gmail.com>
2021-12-02 11:19:08 +0000

    Added new form templates and new main folder

---
+++ Test/BayViewHotel/Forms/Accounts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BayViewHotel.Forms
+{
+    public partial class Accounts : Form
+    {
+        public Accounts()
+        {
+            InitializeComponent();
+        }
+
+        private void Accounts_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'kieranTestDataSet1.Tbl_joel' table. You can move, or remove it, as needed.
+            this.tbl_joelTableAdapter.Fill(this.kieranTestDataSet1.Tbl_joel);
+
+        }
+    }
+}
---
+++ Test/BayViewHotel/Forms/Home.cs
@@ -0,0 +1,31 @@
+﻿using BayViewHotel.Classes;
+using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BayViewHotel.Forms
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+            //IReportServerCredentials creds = new CustomReportCredentials("Administrator", "Cambrian@1", "");
+            NetworkCredential customCred = new NetworkCredential("Administrator", "Cambrian@1", "WIN-41GL1FSD01Q");
+            this.reportViewer1.ServerReport.ReportServerCredentials.NetworkCredentials = customCred;
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
